ab264123a8d0d016a1d3b05a73a5bd2ad483eb14
Ksusha <ks.romanchik@gmail.com>
2017-02-01 16:51:44 +0300

    worked version

--- containers/App.js
+++ containers/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import GameFieldContainer from "./GameFieldContainer";
-import SetAreaSizeContainer from "./SetAreaSizeContainer";
+// import SetAreaSizeContainer from "./SetAreaSizeContainer";
 import SetGameContainer from "./SetGameContainer";
 
 class App extends Component{
@@ -9,7 +9,6 @@ class App extends Component{
 
 		return (
 			<div>
-				<SetAreaSizeContainer /> {/* <gameField> -- container*/}
 				<GameFieldContainer /> {/* <form choose size> -- container*/}
 				
 				<SetGameContainer /> {/* <textarea for json> -- container*/}
